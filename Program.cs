76d0e50ba334990188f2ce668f6d2f72e8c55435
aslanonurcan <aslan.onurcan@yandex.com.tr>
2019-02-23 13:32:28 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoriawithlRecursion
+{
+    class Program
+    {
+        public static int sonuc;
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Lütfen bir sayı giriniz : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Rescursive : {0} ", FaktoriyelRecursive(n));
+            Console.ReadKey();
+        }
+        static int FaktoriyelRecursive(int sayi)
+        {
+            if (sayi > 1)
+            {
+                sonuc = sayi * FaktoriyelRecursive(sayi-1);
+                return sonuc;
+            }
+            else
+            {
+                return sayi;
+            }
+        }
+    }
+}
